ae6e1f104145eebeabece89afeca136ad0e13aa6
zeinbahsein <sayyidhusain14@gmail.com>
2024-09-19 11:51:33 +0700

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title="Home", page_icon="🏠")
 # Mengatur teks agar berada di tengah halaman
 st.markdown("<h1 style='text-align: center;'>👋HOLAA!! SELAMAT DATANG DI WEBSITE ZEIN👋</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'>MAU MELIHAT ANALISA SAYA🔬?</h3>", unsafe_allow_html=True)
