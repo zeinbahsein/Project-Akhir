703d76da4966fb6929c57afba322f55fe6378f85
zeinbahsein <sayyidhusain14@gmail.com>
2024-09-20 10:18:18 +0700

    Menambahkan fitur baru

--- 🏠Home.py
+++ 🏠Home.py
@@ -6,3 +6,5 @@ st.set_page_config(page_title="Home", page_icon="🏠")
 st.markdown("<h1 style='text-align: center;'>👋HOLAA!! SELAMAT DATANG DI WEBSITE ZEIN👋</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'>MAU MELIHAT ANALISA SAYA🔬?</h3>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'>LANGSUNG AJA KLIK HALAMAN 🛠️Project!!</h3>", unsafe_allow_html=True)
+
+#hehe
