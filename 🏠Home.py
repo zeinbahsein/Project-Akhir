adf42bc16a1ba152ce40e8bdb732c39b7279a8dc
zeinbahsein <sayyidhusain14@gmail.com>
2024-09-20 10:19:33 +0700

    hapus baris gk penting

--- 🏠Home.py
+++ 🏠Home.py
@@ -7,4 +7,3 @@ st.markdown("<h1 style='text-align: center;'>👋HOLAA!! SELAMAT DATANG DI WEBSI
 st.markdown("<h3 style='text-align: center;'>MAU MELIHAT ANALISA SAYA🔬?</h3>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'>LANGSUNG AJA KLIK HALAMAN 🛠️Project!!</h3>", unsafe_allow_html=True)
 
-#hehe
